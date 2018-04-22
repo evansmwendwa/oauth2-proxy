2b1046f9ff7ae4d6f9500725184dc40205e5cb82
Evans Mwendwa <evansmwendwa@live.com>
2018-04-22 11:48:18 +0300

    ad dotenv support

--- src/middleware.php
+++ src/middleware.php
@@ -1,11 +1,17 @@
 <?php
 // Application middleware
+use Symfony\Component\Dotenv\Dotenv;
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+$app->add(function ($request, $response, $next) {
+    $dotenv = new Dotenv();
+    $dotenv->load(__DIR__.'/../.env');
+	  return $next($request, $response);
+});
 
 $app->add(new \Slim\Middleware\Session([
     'name' => 'proxy_session',
     'autorefresh' => true,
-    'lifetime' => '5 hours',
+    'lifetime' => getenv('SESSION_LIFETIME'),
     'httponly' => true
 ]));
