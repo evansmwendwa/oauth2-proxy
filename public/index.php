54a869b8a6d4203009775e357d482f9db057624a
Evans Mwendwa <evansmwendwa@users.noreply.github.com>
2018-05-13 20:23:13 +0300

    CORS Fixes and improvements - move cors to middleware - handle OPTIONS responses - handle crossdomain withCredentials - allow header X-XSRF-TOKEN - update default acceptable CORS origin

--- public/index.php
+++ public/index.php
@@ -23,26 +23,6 @@ $dotenv->load(__DIR__.'/../.env');
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
 
-// enable cors in dev mode
-// WARNING for security this proxy is supposed to be served from same origin as your SPA app.
-// make sure CORS is not enables in production
-$debug = (getenv('DEBUG') === 'true');
-
-if($debug) {
-  $app->options('/{routes:.+}', function ($request, $response, $args) {
-      return $response;
-  });
-
-  $app->add(function ($req, $res, $next) {
-      $response = $next($req, $res);
-      return $response
-          ->withHeader('Access-Control-Allow-Origin', getenv('ORIGIN'))
-          ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-          ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  });
-}
-
-
 // Set up dependencies
 require __DIR__ . '/../src/dependencies.php';
 
--- src/middleware.php
+++ src/middleware.php
@@ -7,32 +7,42 @@ use Dflydev\FigCookies\FigResponseCookies;
 // e.g: $app->add(new \Slim\Csrf\Guard);
 
 // middleware for validating csrf tokens from header
-$app->add(function ($request, $response, $next) {
+$app->add(function ($request, $response, $next) use ($app) {
     $session = new \SlimSession\Helper;
 
     if (!$session->exists('csrf_token')) {
-      $csrf_token = bin2hex(random_bytes(60));
-      $session->set('csrf_token', $csrf_token);
+        $csrf_token = bin2hex(random_bytes(60));
+        $session->set('csrf_token', $csrf_token);
     }
 
     $csrf_token = $session->get('csrf_token');
 
     // store csrf token in cookie
-    $response = FigResponseCookies::set($response, SetCookie::create('XSRF-TOKEN')
-        ->withValue($csrf_token)
-        ->withExpires(strtotime(getenv('SESSION_LIFETIME')))
+    $response = FigResponseCookies::set(
+        $response,
+        SetCookie::create('XSRF-TOKEN')
+          ->withValue($csrf_token)
+          ->withExpires(strtotime(getenv('SESSION_LIFETIME')))
     );
 
     $clientCsrfToken = $request->getHeaderLine('X-XSRF-TOKEN');
 
     if(empty($clientCsrfToken) || $csrf_token !== $clientCsrfToken) {
-      return $response->withJson([
-        'authenticated' => 'false',
-        'token' => [
-          'error' => 'csrf_validation_failure',
-          'message' => 'Missing or invalid csrf token'
-        ]
-      ]);
+        $response = $response->withJson([
+          'authenticated' => false,
+          'token' => [
+            'error' => 'csrf_validation_failure',
+            'message' => 'Missing or invalid csrf token'
+          ]
+        ]);
+
+        $method = $request->getMethod();
+
+        if(strtolower($method) === 'options') {
+            return $next($request, $response);
+        } else {
+            return $response;
+        }
     }
 
     return $next($request, $response);
@@ -45,3 +55,25 @@ $app->add(new \Slim\Middleware\Session([
     'lifetime' => getenv('SESSION_LIFETIME'),
     'httponly' => true
 ]));
+
+/**
+ * enable CORS in dev mode
+ * WARNING for security this proxy is supposed to be served
+ * from same origin as your SPA app without CORS enabled
+ * make sure CORS is not enabled in production
+ **/
+$app->add(function ($request, $response, $next) {
+    $debug = (getenv('DEBUG') === 'true');
+
+    if($debug) {
+        $corsResponse = $response
+            ->withHeader('Access-Control-Allow-Origin', getenv('ORIGIN'))
+            ->withHeader('Access-Control-Allow-Credentials', 'true')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization, X-XSRF-TOKEN, withCredentials')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
+        return $next($request, $corsResponse);
+    }
+
+    return $next($request, $response);
+});
